2d182ba98446cb972fa08e898f8818b2792d9f0a
dahye lim <hapwoo@gmail.com>
2022-04-20 16:20:31 +0900

    pagination

--- Brewery/BeerListViewController.swift
+++ Brewery/BeerListViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class BeerListViewController: UITableViewController {
     var beerList = [Beer]()
+    var dataTasks = [URLSessionTask]()
     var currentPage = 1
     
     override func viewDidLoad() {
@@ -21,13 +22,15 @@ class BeerListViewController: UITableViewController {
         //UITableView 설정
         tableView.register(BeerListCell.self, forCellReuseIdentifier: "BeerListCell")
         tableView.rowHeight = 150
+        tableView.prefetchDataSource = self
         
         fetchBeer(of: currentPage)
     }
 }
 
 //UITabelView DataSource, Delegate
-extension BeerListViewController {
+extension BeerListViewController: UITableViewDataSourcePrefetching {
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return beerList.count
     }
@@ -49,12 +52,24 @@ extension BeerListViewController {
         detailViewController.beer = selectedBeer
         self.show(detailViewController, sender: nil)
     }
+    
+    func tableView(_ tableView: UITableView, prefetchRowsAt indexPaths: [IndexPath]) {
+        
+        guard currentPage != 1 else { return }
+        indexPaths.forEach {
+            if ($0.row + 1)/25 + 1 == currentPage {
+                self.fetchBeer(of: currentPage)
+            }
+        }
+    }
 }
 
 //Data Fetching
 private extension BeerListViewController {
     func fetchBeer(of page: Int) {
-        guard let url = URL(string: "https://api.punkapi.com/v2/beers?page=\(page)") else { return }
+        guard let url = URL(string: "https://api.punkapi.com/v2/beers?page=\(page)"),
+              dataTasks.firstIndex(where: { $0.originalRequest?.url == url }) == nil
+            else { return }
         
         var request = URLRequest(url: url)
         request.httpMethod = "GET"
@@ -96,5 +111,6 @@ private extension BeerListViewController {
         })
         
         dataTask.resume()
+        dataTasks.append(dataTask)
     }
 }
